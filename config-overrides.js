f12856c629540484922f8adc7a70efb6e3a450fb
Dmitry Kartsev <dkartsev@redhat.com>
2019-02-10 22:20:20 +0200

    update colors schema

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,18 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
 module.exports = function override(config, env) {
-    config = injectBabelPlugin(
-        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-        config,
-    );
+    config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config);
+    config = rewireLess.withLoaderOptions({
+      javascriptEnabled: true,
+      modifyVars: {
+        "@primary-color": "#007e88",
+        "@btn-primary-bg": "#eb6710",
+        "@layout-body-background": "#fff",
+        "@layout-header-background":"#b1d9ff"
+      }
+    })(config, env);
+
     return config;
 };
 
