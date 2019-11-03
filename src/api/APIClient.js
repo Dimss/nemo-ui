34f9a8e8dbc00bad749d718ee2c7e634471e9744
Dmitry Kartsev <dkartsev@redhat.com>
2019-11-03 23:43:18 +0200

    update services urls

--- src/api/APIClient.js
+++ src/api/APIClient.js
@@ -1,66 +1,66 @@
 import axios from "axios";
-import { store } from "../store";
+import {store} from "../store";
 
 export default class ApiClient {
-  constructor() {
-    this.identity = "http://identity.nemo";
-    this.receiver = "http://receiver.nemo";
-    this.feed = "http://feed.nemo";
-  }
+    constructor() {
+        this.identity = "http://identity.apps.a.okto.ocplab.io";
+        this.receiver = "http://receiver.apps.a.okto.ocplab.io";
+        this.feed = "http://feed.apps.a.okto.ocplab.io";
+    }
 
-  fetchUserFeed() {
-    return this.execRequest("get", this.feed + "/feed");
-  }
+    fetchUserFeed() {
+        return this.execRequest("get", this.feed + "/feed");
+    }
 
-  deleteLink(imageId) {
-    return this.execRequest("delete", this.feed + "/links/" + imageId);
-  }
+    deleteLink(imageId) {
+        return this.execRequest("delete", this.feed + "/links/" + imageId);
+    }
 
-  addComment(comment, imageId) {
-    let payload = { imageId: imageId, comment: comment };
-    return this.execRequest("post", this.feed + "/comments", payload);
-  }
+    addComment(comment, imageId) {
+        let payload = {imageId: imageId, comment: comment};
+        return this.execRequest("post", this.feed + "/comments", payload);
+    }
 
-  userLogin(email, password) {
-    let payload = { email: email, password: password };
-    return this.execRequest("post", this.identity + "/v1/login", payload);
-  }
+    userLogin(email, password) {
+        let payload = {email: email, password: password};
+        return this.execRequest("post", this.identity + "/v1/login", payload);
+    }
 
-  userSignup(email, password) {
-    let payload = { email: email, password: password };
-    return this.execRequest("post", this.identity + "/v1/signup", payload);
-  }
+    userSignup(email, password) {
+        let payload = {email: email, password: password};
+        return this.execRequest("post", this.identity + "/v1/signup", payload);
+    }
 
-  validateToekn(token) {
-    return this.execRequest("get", this.identity + "/v1/token/" + token);
-  }
+    validateToekn(token) {
+        return this.execRequest("get", this.identity + "/v1/token/" + token);
+    }
 
-  uploadImage(data) {
-    return this.execRequest("post", this.receiver + "/upload", data, true);
-  }
+    uploadImage(data) {
+        return this.execRequest("post", this.receiver + "/upload", data, true);
+    }
 
-  addLike(imageId) {
-    return this.execRequest("post", this.feed + "/likes/" + imageId);
-  }
+    addLike(imageId) {
+        return this.execRequest("post", this.feed + "/likes/" + imageId);
+    }
 
-  execRequest(method, url, data = null, isFromData = false) {
-    //Try to login the token from State
-    let token = store.getState().loginReducer.token;
-    // If not found in state (WTF?!) try to load it from localStorage
-    if (!token) token = localStorage.getItem("token");
-    let headers = { "X-NEMO-AUTH": token };
-    // if (isFromData) headers["Content-Type"] = "multipart/form-data";
-    return axios({
-      method: method,
-      url: url,
-      headers: headers,
-      data: data
-    })
-      .then(resData => {
-        return Promise.resolve([null, resData]);
-      })
-      .catch(error => {
-        return Promise.resolve([error]);
-      });
-  }
+    execRequest(method, url, data = null, isFromData = false) {
+        //Try to login the token from State
+        let token = store.getState().loginReducer.token;
+        // If not found in state (WTF?!) try to load it from localStorage
+        if (!token) token = localStorage.getItem("token");
+        let headers = {"X-NEMO-AUTH": token};
+        // if (isFromData) headers["Content-Type"] = "multipart/form-data";
+        return axios({
+            method: method,
+            url: url,
+            headers: headers,
+            data: data
+        })
+            .then(resData => {
+                return Promise.resolve([null, resData]);
+            })
+            .catch(error => {
+                return Promise.resolve([error]);
+            });
+    }
 }
